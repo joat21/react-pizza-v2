ab98e3ba30ea0b01eb7cc6f55cae28a7429fae1a
joat21 <eridel61@yandex.ru>
2025-06-15 17:53:59 +0500

    minor changes
    Added favicon
    Added .env.production
    Added empty placeholder for orders list in profile

--- src/modules/Profile/components/OrdersList.tsx
+++ src/modules/Profile/components/OrdersList.tsx
@@ -11,34 +11,38 @@ export const OrdersList = () => {
   return (
     <div>
       <h2 className="text-3xl font-semibold leading-8">Мои заказы</h2>
-      <ul className="flex flex-col gap-9 py-10 px-8">
-        {orders.map((order, index) => (
-          <li
-            key={order.id}
-            className="max-w-3xl rounded-3xl bg-white shadow-lg"
-          >
-            <div className="flex items-center gap-5 py-7 px-9 border-b border-gray-200">
-              <h3 className="text-2xl font-bold">
-                Заказ #{orders.length - index}
-              </h3>
-              <span className="text-gray-500">
-                {formatDate(order.createdAt)}
-              </span>
-            </div>
-            <ul>
-              {order.items.map((item) => (
-                <li key={item.id} className="group">
-                  <OrderItem item={item} />
-                </li>
-              ))}
-            </ul>
-            <div className="py-7 px-9 border-t border-gray-200 text-2xl">
-              Итого:
-              <span className="font-extrabold"> {order.totalPrice} ₽</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {orders.length ? (
+        <ul className="flex flex-col gap-9 py-10 px-8">
+          {orders.map((order, index) => (
+            <li
+              key={order.id}
+              className="max-w-3xl rounded-3xl bg-white shadow-lg"
+            >
+              <div className="flex items-center gap-5 py-7 px-9 border-b border-gray-200">
+                <h3 className="text-2xl font-bold">
+                  Заказ #{orders.length - index}
+                </h3>
+                <span className="text-gray-500">
+                  {formatDate(order.createdAt)}
+                </span>
+              </div>
+              <ul>
+                {order.items.map((item) => (
+                  <li key={item.id} className="group">
+                    <OrderItem item={item} />
+                  </li>
+                ))}
+              </ul>
+              <div className="py-7 px-9 border-t border-gray-200 text-2xl">
+                Итого:
+                <span className="font-extrabold"> {order.totalPrice} ₽</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span>Вы еще не сделали не одного заказа</span>
+      )}
     </div>
   );
 };
